17be21aa5871cf31218216ccabb9bf68c51d4295
Sayan25112002 <sayanmajumdar1920014@gmail.com>
2026-08-22 15:10:00 +0530

    CommentServiceImpl Added, CommentRepository Modified

--- src/main/java/com/build/ECommerce/service/implementation/CommentServiceImpl.java
+++ src/main/java/com/build/ECommerce/service/implementation/CommentServiceImpl.java
@@ -1,4 +1,48 @@
 package com.build.ECommerce.service.implementation;
 
-public class CommentServiceImpl {
+import com.build.ECommerce.dto.requestDto.CommentRequestDto;
+import com.build.ECommerce.dto.responseDto.CommentResponseDto;
+import com.build.ECommerce.entity.Comment;
+import com.build.ECommerce.entity.Product;
+import com.build.ECommerce.entity.User;
+import com.build.ECommerce.exception.ResourceNotFoundException;
+import com.build.ECommerce.mapper.CommentMapper;
+import com.build.ECommerce.repository.CommentRepository;
+import com.build.ECommerce.repository.ProductRepository;
+import com.build.ECommerce.repository.UserRepository;
+import com.build.ECommerce.service.CommentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class CommentServiceImpl implements CommentService {
+
+    private final CommentRepository commentRepository;
+    private final CommentMapper commentMapper;
+    private final ProductRepository productRepository;
+    private final UserRepository userRepository;
+
+    @Override
+    public CommentResponseDto addComment(CommentRequestDto commentRequestDto, Long productId, Long userId) {
+        Product product = productRepository.findById(productId).orElseThrow(() -> new ResourceNotFoundException("Product Not Found"));
+        User user = userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("User Not Found"));
+        Comment comment = commentMapper.toComment(commentRequestDto);
+        comment.setProduct(product);
+        comment.setUser(user);
+        commentRepository.save(comment);
+        return commentMapper.toCommentResponseDto(comment);
+    }
+
+    @Override
+    public List<CommentResponseDto> getCommentsByProduct(Long productId) {
+        List<Comment> comments = commentRepository.findByProductId(productId);
+        return comments
+                .stream()
+                .map(commentMapper::toCommentResponseDto)
+                .collect(Collectors.toList());
+    }
 }
