94efdee3474a7cbebff8f3b9655afab9de99b7bc
Sayan25112002 <sayanmajumdar1920014@gmail.com>
2026-08-22 17:03:13 +0530

    CartMapper Modified

--- src/main/java/com/build/ECommerce/mapper/CartMapper.java
+++ src/main/java/com/build/ECommerce/mapper/CartMapper.java
@@ -2,9 +2,11 @@ package com.build.ECommerce.mapper;
 
 import com.build.ECommerce.dto.responseDto.CartResponseDto;
 import com.build.ECommerce.entity.Cart;
+import org.mapstruct.Mapper;
 
 import java.util.List;
 
+@Mapper(componentModel = "spring")
 public interface CartMapper {
 
     CartResponseDto toCartResponseDto(Cart cart);
--- src/main/java/com/build/ECommerce/service/implementation/CartServiceImpl.java
+++ src/main/java/com/build/ECommerce/service/implementation/CartServiceImpl.java
@@ -1,9 +1,26 @@
 package com.build.ECommerce.service.implementation;
 
+import com.build.ECommerce.dto.responseDto.CartResponseDto;
+import com.build.ECommerce.mapper.CartMapper;
+import com.build.ECommerce.repository.CartRepository;
+import com.build.ECommerce.service.CartService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class CartServiceImpl {
+public class CartServiceImpl implements CartService {
+
+    private final CartRepository cartRepository;
+    private final CartMapper cartMapper;
+
+    @Override
+    public CartResponseDto addCart(Long userId, Long productId, Integer quantity) {
+        return null;
+    }
+
+    @Override
+    public void clearCart(Long userId) {
+
+    }
 }
