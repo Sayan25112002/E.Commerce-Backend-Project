318535d2d6813213a879990adc84d5b19d1679f7
Sayan25112002 <sayanmajumdar1920014@gmail.com>
2026-08-24 18:30:09 +0530

    ErrorDetails Added

---
+++ src/main/java/com/build/ECommerce/dto/responseDto/ErrorDetails.java
@@ -0,0 +1,22 @@
+package com.build.ECommerce.dto.responseDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ErrorDetails {
+
+    private Date timestamp;
+
+    private String message;
+
+    private String error;
+
+}
