e91c7797de04c8f95833bac7f1455a277fbe599d
Sayan25112002 <sayanmajumdar1920014@gmail.com>
2026-08-24 15:29:48 +0530

    OrderService and OrderServiceImpl Added

--- src/main/java/com/build/ECommerce/service/implementation/OrderServiceImpl.java
+++ src/main/java/com/build/ECommerce/service/implementation/OrderServiceImpl.java
@@ -1,5 +1,6 @@
 package com.build.ECommerce.service.implementation;
 
+import com.build.ECommerce.dto.responseDto.OrderResponseDto;
 import com.build.ECommerce.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -8,5 +9,8 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
-
+    @Override
+    public OrderResponseDto createOrder(Long userId, String address, String phoneNumber) {
+        return null;
+    }
 }
