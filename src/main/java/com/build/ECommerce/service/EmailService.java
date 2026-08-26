761240e3f81fe6843c7260cb869be3c5bb9f4a1e
Sayan25112002 <sayanmajumdar1920014@gmail.com>
2026-08-26 17:44:20 +0530

    EmailService Modiified

--- src/main/java/com/build/ECommerce/service/EmailService.java
+++ src/main/java/com/build/ECommerce/service/EmailService.java
@@ -1,6 +1,7 @@
 package com.build.ECommerce.service;
 
 import com.build.ECommerce.entity.Order;
+import com.build.ECommerce.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
@@ -22,6 +23,15 @@ public class EmailService {
         message.setTo("vhadjz5965@minitts.net");
         message.setSubject("Order Confirmation");
         message.setText("Your Order has been confirmed. Order Id : "+order.getId());
+        mailSender.send(message);
+    }
+
+    public void sendEmailConfirmation(User user) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(fromEmail);
+        message.setTo(user.getEmail());
+        message.setSubject("Confirm your Email");
+        message.setText("Please confirm your email by entering this code "+user.getConfirmationCode());
         mailSender.send(message);
     }
 }
